52f4b4ebb4af9c47b498af57b9ff7e7921c085f9
Tomasz Iwanicki <tiko35@o2.pl>
2023-04-19 23:08:21 +0200

    fix 1

--- src/main/java/pl/javastart/task/FileUtils.java
+++ src/main/java/pl/javastart/task/FileUtils.java
@@ -1,7 +1,6 @@
 package pl.javastart.task;
 
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,28 +9,28 @@ import java.util.Scanner;
 public class FileUtils {
     static List<Product> readProducts(String fileName) throws FileNotFoundException {
         List<Product> products = new ArrayList<>();
-        InputStream inputStream = FileUtils.class.getClassLoader().getResourceAsStream(fileName);
-        Scanner scanner = new Scanner(inputStream);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] split = line.split(";");
-            BigDecimal amount = new BigDecimal(split[1]);
-            products.add(new Product(split[0], amount, split[2]));
+        try (Scanner scanner = new Scanner(FileUtils.class.getClassLoader().getResourceAsStream(fileName))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] split = line.split(";");
+                BigDecimal amount = new BigDecimal(split[1]);
+                products.add(new Product(split[0], amount, split[2]));
+            }
+            return products;
         }
-        return products;
     }
 
     static List<Currency> readCurrencies(String fileName) throws FileNotFoundException {
         List<Currency> currencies = new ArrayList<>();
-        InputStream inputStream = FileUtils.class.getClassLoader().getResourceAsStream(fileName);
-        Scanner scanner = new Scanner(inputStream);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] split = line.split(";");
-            BigDecimal rate = new BigDecimal(split[1]);
-            currencies.add(new Currency(split[0], rate));
+        try (Scanner scanner = new Scanner(FileUtils.class.getClassLoader().getResourceAsStream(fileName))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] split = line.split(";");
+                BigDecimal rate = new BigDecimal(split[1]);
+                currencies.add(new Currency(split[0], rate));
+            }
+            scanner.close();
+            return currencies;
         }
-        scanner.close();
-        return currencies;
     }
 }
